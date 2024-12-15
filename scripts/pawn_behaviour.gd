517dd18aee31cfaeec02c23006382549baf97ad9
Fouad Atwi <atwe98@gmail.com>
2024-12-15 23:32:14 +0100

    slight optimziation for pawn_behaviour

--- scripts/pawn_behaviour.gd
+++ scripts/pawn_behaviour.gd
@@ -1,16 +1,18 @@
 extends Behaviour
 
 
-func get_valid_moves(board_position: Vector2i, forward_direction: int, first_move: bool = false) -> Array[Vector2i]:
+func get_valid_moves(coords: Vector2i, forward_direction: int, first_move: bool = false) -> Array[Vector2i]:
 	var moves: Array[Vector2i] = []
-	var pos := board_position
-	pos.y += 1*forward_direction
-	moves.append(pos)
+	coords.y += forward_direction
+	if Utils.is_within_bounds(coords):
+		moves.append(coords)
+	
 
 	# Check for double step
 	if first_move:
 		# TODO: solve special kill
-		pos.y += 1*forward_direction
-		moves.append(pos)
+		coords.y += forward_direction
+		if Utils.is_within_bounds(coords):
+			moves.append(coords)
 
 	return moves
